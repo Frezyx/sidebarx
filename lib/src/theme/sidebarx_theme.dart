61b199bc18e1f7dab4ed123410c1b536daa4f97a
Maurizio Sodano <maurizio.sodano@gmail.com>
2023-06-13 08:02:57 +0200

    #48 -format

--- lib/src/theme/sidebarx_theme.dart
+++ lib/src/theme/sidebarx_theme.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 
 class SidebarXTheme {
-  const SidebarXTheme(
-      {this.width = 70,
-      this.height = double.infinity,
-      this.padding = EdgeInsets.zero,
-      this.margin = EdgeInsets.zero,
-      this.decoration,
-      this.iconTheme,
-      this.selectedIconTheme,
-      this.textStyle,
-      this.selectedTextStyle,
-      this.itemDecoration,
-      this.selectedItemDecoration,
-      this.itemMargin,
-      this.selectedItemMargin,
-      this.itemPadding,
-      this.selectedItemPadding,
-      this.itemTextPadding,
-      this.selectedItemTextPadding,
-      this.hoverColor,
-      this.hoverTextStyle});
+  const SidebarXTheme({
+    this.width = 70,
+    this.height = double.infinity,
+    this.padding = EdgeInsets.zero,
+    this.margin = EdgeInsets.zero,
+    this.decoration,
+    this.iconTheme,
+    this.selectedIconTheme,
+    this.textStyle,
+    this.selectedTextStyle,
+    this.itemDecoration,
+    this.selectedItemDecoration,
+    this.itemMargin,
+    this.selectedItemMargin,
+    this.itemPadding,
+    this.selectedItemPadding,
+    this.itemTextPadding,
+    this.selectedItemTextPadding,
+    this.hoverColor,
+    this.hoverTextStyle,
+  });
 
   /// [SidebarX] width
   final double width;
@@ -163,26 +164,27 @@ class SidebarXTheme {
     TextStyle? hoverTextStyle,
   }) {
     return SidebarXTheme(
-        width: width ?? this.width,
-        height: height ?? this.height,
-        padding: padding ?? this.padding,
-        margin: margin ?? this.margin,
-        decoration: decoration ?? this.decoration,
-        iconTheme: iconTheme ?? this.iconTheme,
-        selectedIconTheme: selectedIconTheme ?? this.selectedIconTheme,
-        textStyle: textStyle ?? this.textStyle,
-        selectedTextStyle: selectedTextStyle ?? this.selectedTextStyle,
-        itemDecoration: itemDecoration ?? this.itemDecoration,
-        selectedItemDecoration:
-            selectedItemDecoration ?? this.selectedItemDecoration,
-        itemMargin: itemMargin ?? this.itemMargin,
-        selectedItemMargin: selectedItemMargin ?? this.selectedItemMargin,
-        itemPadding: itemPadding ?? this.itemPadding,
-        selectedItemPadding: selectedItemPadding ?? this.selectedItemPadding,
-        itemTextPadding: itemTextPadding ?? this.itemTextPadding,
-        selectedItemTextPadding:
-            selectedItemTextPadding ?? this.selectedItemTextPadding,
-        hoverColor: hoverColor ?? this.hoverColor,
-        hoverTextStyle: hoverTextStyle ?? this.hoverTextStyle);
+      width: width ?? this.width,
+      height: height ?? this.height,
+      padding: padding ?? this.padding,
+      margin: margin ?? this.margin,
+      decoration: decoration ?? this.decoration,
+      iconTheme: iconTheme ?? this.iconTheme,
+      selectedIconTheme: selectedIconTheme ?? this.selectedIconTheme,
+      textStyle: textStyle ?? this.textStyle,
+      selectedTextStyle: selectedTextStyle ?? this.selectedTextStyle,
+      itemDecoration: itemDecoration ?? this.itemDecoration,
+      selectedItemDecoration:
+          selectedItemDecoration ?? this.selectedItemDecoration,
+      itemMargin: itemMargin ?? this.itemMargin,
+      selectedItemMargin: selectedItemMargin ?? this.selectedItemMargin,
+      itemPadding: itemPadding ?? this.itemPadding,
+      selectedItemPadding: selectedItemPadding ?? this.selectedItemPadding,
+      itemTextPadding: itemTextPadding ?? this.itemTextPadding,
+      selectedItemTextPadding:
+          selectedItemTextPadding ?? this.selectedItemTextPadding,
+      hoverColor: hoverColor ?? this.hoverColor,
+      hoverTextStyle: hoverTextStyle ?? this.hoverTextStyle,
+    );
   }
 }
